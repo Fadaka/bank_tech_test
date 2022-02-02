f1d5bcd3f7d7ccee511f7f9d20f6a132f9982a73
Fadaka <master_kunle14@hotmail.co.uk>
2022-02-02 07:50:50 +0000

    formatted code

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,48 +1,48 @@
+# frozen_string_literal: true
+
 class Account
-    
-    attr_reader :balance, :debit, :credit, :transac_time, :debit_history, :credit_history, :balance_history,:transac_history
+  attr_reader :balance, :debit, :credit, :transac_time, :debit_history, :credit_history, :balance_history,
+              :transac_history
 
-    def initialize 
-        @balance = 0
-        @debit_history = []
-        @credit_history = []
-        @balance_history = []
-        @transac_history = []
-    end
+  def initialize
+    @balance = 0
+    @debit_history = []
+    @credit_history = []
+    @balance_history = []
+    @transac_history = []
+  end
 
-    def debit(amount)
-        @balance += amount
-        @debit_history.push(amount)
-        @credit_history.push(" ")
-        @balance_history.push(@balance)
-        transac_time
-        return @balance
-    end
+  def debit(amount)
+    @balance += amount
+    @debit_history.push(amount)
+    @credit_history.push(' ')
+    @balance_history.push(@balance)
+    transac_time
+    @balance
+  end
 
-    def credit(amount)
+  def credit(amount)
+    raise 'insufficient balance in account' if amount > @balance
 
-        if amount> @balance
-            raise 'insufficient balance in account'
-        end
-         @balance -= amount
-         @credit_history.push(amount)
-         @debit_history.push(" ")
-         @balance_history.push(@balance)
-         transac_time
-        return @balance 
-    end
- 
-    def transac_time
-        t = Time.now
-        date_array = (@balance_history).map {t}
-        date_array_formatted = date_array.map {|date| date.strftime("%d-%m-%Y")}
-        transac_history.push(date_array_formatted[-1])
-    end
+    @balance -= amount
+    @credit_history.push(amount)
+    @debit_history.push(' ')
+    @balance_history.push(@balance)
+    transac_time
+    @balance
+  end
 
-    def statement
-        statment_array = [@transac_history, @credit_history, @debit_history, @balance_history]
-        bank_statement = statment_array.transpose()
-        print ('[DATE, CREDIT, DEBIT, BALANCE]')
-        return bank_statement
-    end
+  def transac_time
+    t = Time.now
+    date_array = @balance_history.map { t }
+    date_array_formatted = date_array.map { |date| date.strftime('%d-%m-%Y') }
+    transac_history.push(date_array_formatted[-1])
+  end
+
+  def statement
+    statment_array = [@transac_history, @credit_history, @debit_history, @balance_history]
+    bank_statement = statment_array.transpose
+    print('[DATE, CREDIT, DEBIT, BALANCE]')
+    bank_statement
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,54 +1,52 @@
+# frozen_string_literal: true
+
 require 'bank'
 
 describe Account do
+  let(:santander) { described_class.new }
 
-    let (:santander) {described_class.new}
+  it ' will check that an amount can be deposite into account' do
+    santander.debit(300)
+    expect(santander.balance).to eq(300)
+  end
 
-    it ' will check that an amount can be deposite into account' do
-        santander.debit(300)
-        expect(santander.balance).to eq(300)
-    end
+  it 'will check that an amount can be credited from an account ' do
+    santander.debit(300)
+    santander.credit(100)
+    expect(santander.balance).to eq(200)
+  end
+  it 'will raise an error ,if money is credited more than balance' do
+    santander.debit(10)
+    expect { santander.credit(100) }.to raise_error('insufficient balance in account')
+  end
 
-    it 'will check that an amount can be credited from an account 'do
-        santander.debit(300)
-        santander.credit(100)
-        expect(santander.balance).to eq(200)
-    end
-    it 'will raise an error ,if money is credited more than balance'do
-        santander.debit(10)
-        expect{ santander.credit(100) }.to raise_error('insufficient balance in account')
+  it 'will check that the debit has been recorded' do
+    santander.debit(10)
+    expect(santander.debit_history).to match_array([10])
+  end
 
-    end
+  it 'will check that the credit and debit has been recorded' do
+    santander.debit(100)
+    santander.credit(100)
+    expect(santander.credit_history).to match_array([' ', 100])
+  end
 
-    it 'will check that the debit has been recorded' do
-        santander.debit(10)
-        expect(santander.debit_history).to match_array([10])
-    end
+  it 'will check that the balance has been recorded' do
+    santander.debit(100)
+    santander.credit(30)
+    expect(santander.balance_history).to match_array([100, 70])
+  end
 
-    it 'will check that the credit and debit has been recorded' do
-        santander.debit(100)
-        santander.credit(100)
-        expect(santander.credit_history).to match_array([" ",100])
-    end
+  it 'will check that the transaction time has been recorded' do
+    t = Time.now
+    santander.debit(100)
+    expect(santander.statement[0][0]).to eq(t.strftime('%d-%m-%Y'))
+  end
 
-
-    it 'will check that the balance has been recorded' do
-        santander.debit(100)
-        santander.credit(30)
-        expect(santander.balance_history).to match_array([100, 70])
-        
-    end
-
-    it 'will check that the transaction time has been recorded' do
-        t = Time.now
-        santander.debit(100)
-        expect(santander.statement[0][0]).to eq(t.strftime("%d-%m-%Y"))
-    end
-
-    it 'will return a complete bank statement of transaction history' do
-        t = Time.now
-        time_now = t.strftime("%d-%m-%Y")
-        santander.debit(100)
-        expect(santander.statement).to include([time_now, " ", 100, 100])
-    end
+  it 'will return a complete bank statement of transaction history' do
+    t = Time.now
+    time_now = t.strftime('%d-%m-%Y')
+    santander.debit(100)
+    expect(santander.statement).to include([time_now, ' ', 100, 100])
+  end
 end
